53da53af7c912cdec66c0427eaf086cbf183c5a1
DunhuangS <2003358@apps.nsd.org>
2025-09-12 16:46:32 +0000

    mastering println

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,23 @@
-// Student name
-// class period
-// project name
-// date
+// Dunhuang Su
+// P2
+// Zero Hero
+// 9/12/2025
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber says
+ * INPUT: we don't know about input yet
+ * OUTPUT: Some cool words in the command line terminal thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
-package edu.bhscs;
+ package edu.bhscs;
+
+ class Main {
+  public static void main (String[] args) {
+    System.out.println("------------");
+    System.out.println("!!! program start !!!");
+    System.out.println("hello");
+    System.out.println("!!! program end !!!");
+    System.out.println("------------");
+  }
+ }
