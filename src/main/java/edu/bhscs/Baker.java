010e91644ea7af5ce4d8c26a54258705b19eba89
DunhuangS <2003358@apps.nsd.org>
2025-10-08 09:51:29 -0700

    used given code

--- src/main/java/edu/bhscs/Baker.java
+++ src/main/java/edu/bhscs/Baker.java
@@ -79,7 +79,7 @@ public class Baker {
   }
 
   void takeJob(Store bakery) {
-    String doYouWantToWorkHere = this.p.giveAnswer("Do you want to work at " + bakery.getName());
+    String doYouWantToWorkHere = this.p.giveAnswer("Do you want to work at " + bakery.getName() + "? (y/n)");
     if (doYouWantToWorkHere.equals("y")) {
       this.placeOfWork = bakery;
       System.out.println(this.name + " now works at " + bakery.getName());
