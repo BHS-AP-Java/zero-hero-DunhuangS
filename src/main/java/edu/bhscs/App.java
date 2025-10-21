c078ae6e84f213ff0fd16f69b77627bf25fff8ad
DunhuangS <2003358@apps.nsd.org>
2025-10-21 09:26:50 -0700

    Hey uh I understand how to make an app file so yeah commit that YAYYYY

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,12 @@
+package edu.bhscs;
+
+public class App {
+  public static void main(String[] args) {
+    date_10_21();
+    System.out.println("I'm the app!!!!!!!!");
+  }
+
+  public static void date_10_21() {
+    System.out.println("Toedale work");
+  }
+}
